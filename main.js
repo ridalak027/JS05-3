be0280b1b46d4accb210b496f8a9e6e9f73c8ad8
ridalak027 <132017745+ridalak027@users.noreply.github.com>
2023-06-20 18:51:31 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
-/* function 1 */
+/* function 1 (version 1)*/
+var N = "N";
 
-var N = 1;
-var txt = "";
-
-function verf (numb1) {
-  if(isNaN(numb1)){
-    txt = "h";
-    console.log(txt);
-    document.getElementById("stat").innerHTML= "not";
-  }else{
-    txt = "hh";
-    console.log(txt);
-    document.getElementById("stat").innerHTML= "yes";
-  }      
+function verf(numb1) {
+  if (isNaN(numb1)) {
+    document.getElementById("stat").innerHTML = `${numb1} :is not a number`;
+  } else {
+    document.getElementById("stat").innerHTML = `${numb1} :yes it is a number`;
+  }
 }
 
 verf(N);
@@ -22,66 +16,77 @@ verf(N);
 var N1 = 1;
 var N2 = 3;
 
-function verf_two(numb1,numb2) {
-  if(N1 > N2){
-    document.getElementById("grat").innerHTML= N1;
-  }else if(N2 > N1){
-    document.getElementById("grat").innerHTML= N2;
-  }else{
-    document.getElementById("grat").innerHTML= N1=N2;
+function verf_two(numb1, numb2) {
+  if (N1 > N2) {
+    document.getElementById("grat").innerHTML = `the great number is: ${N1}`;
+  } else if (N2 > N1) {
+    document.getElementById("grat").innerHTML = `the great number is: ${N2}`;
+  } else {
+    document.getElementById("grat").innerHTML = `the two numbers are equales`;
   }
 }
 
-verf_two(N1,N2);
+verf_two(N1, N2);
 
 /* function 3 */
 
-const TAB =[1,2,3,4,5,6,7,8,9,10];
-var txt ="";
+const TAB = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var txt = "";
 
 function print_even(T) {
-  
-  for(var i=0 ; i < 10 ; i++){
-
-    if(TAB[i] % 2 ==0){
-      txt += TAB[i]  + " ";
+  for (var i = 0; i < 10; i++) {
+    if (TAB[i] % 2 == 0) {
+      txt += TAB[i] + " ";
     }
-
   }
-  document.getElementById("tab").innerHTML=txt;
+  document.getElementById(
+    "tab"
+  ).innerHTML = `the evens numbers in the table are: ${txt}`;
 }
 
 print_even(TAB);
 
 /* function 4 */
 
-
 function print_date() {
-
   const datt = new Date();
-  var days = datt.getDate();;
-  var month = datt.getMonth()+1;
+  var days = datt.getDate();
+  var month = datt.getMonth() + 1;
   var year = datt.getFullYear();
   var day = datt.getDay();
 
-  if(day == 0){
+  if (day == 0) {
     day = "Sunday";
-  }else if(day == 1){
+  } else if (day == 1) {
     day = "Monday";
-  }else if(day ==2){
-    day = "Tuesday" ;
-  }else if(day ==3){
-    day = "Wednesday";   
-  }else if(day ==4){
+  } else if (day == 2) {
+    day = "Tuesday";
+  } else if (day == 3) {
+    day = "Wednesday";
+  } else if (day == 4) {
     day = "Thursday";
-  }else if(day ==5){
+  } else if (day == 5) {
     day = "Friday";
-  }else{
+  } else {
     day = Saturday;
   }
 
-  document.getElementById("date").innerHTML= `To day is: ${day},${days}/${month}/${year}`;
-
+  document.getElementById(
+    "date"
+  ).innerHTML = `To day is: ${day},${days}/${month}/${year}`;
 }
 
 print_date();
+
+/* function 2 (version 2) */
+
+function verf_v2() {
+  var x = document.getElementById("inputtxt").value;
+  if (isNaN(x)) {
+    document.getElementById("out").innerHTML = `${x} :is not a number`;
+  } else {
+    document.getElementById("out").innerHTML = `${x} :yes it is a number`;
+  }
+}
+
+/*  */
